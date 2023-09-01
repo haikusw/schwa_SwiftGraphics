1f4a7bfb118d307018b89427575056d7a233ad08
Jonathan Wight <jwight@mac.com>
2023-09-01 15:05:05 -0700

    Initial dump of code from Everything.

--- Package.swift
+++ Package.swift
@@ -1,21 +1,34 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftGraphics",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .macCatalyst(.v15),
+        .tvOS(.v16),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftGraphics",
             targets: ["SwiftGraphics"]),
     ],
+    dependencies: [
+        .package(url: "http://github.com/schwa/CoreGraphicsGeometrySupport", from: "0.1.0"),
+        .package(url: "http://github.com/schwa/Everything", branch: "jwight/develop"),
+        .package(url: "https://github.com/schwa/SIMD-Support.git", from: "0.1.2"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftGraphics"),
+            name: "SwiftGraphics",
+            dependencies: [
+                .product(name: "CoreGraphicsGeometrySupport", package: "CoreGraphicsGeometrySupport"),
+                .product(name: "Everything", package: "Everything"),
+                .product(name: "SIMDSupport", package: "SIMD-Support"),
+            ]
+        ),
         .testTarget(
             name: "SwiftGraphicsTests",
             dependencies: ["SwiftGraphics"]),
